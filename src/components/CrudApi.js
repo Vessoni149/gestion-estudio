1c5f01770483023907af77e972672e414e3fde38
Agustin Vessoni <agustinvessoni149@gmail.com>
2023-05-17 08:52:51 -0600

    cambio de url

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -17,7 +17,7 @@ export const CrudApi = () => {
   // const [horasPorMateria, setHorasPorMateria] = useState({});
   const [openModal, setOpenModal] = useState(false);
   let api = helpHttp();
-  let url = "http://localhost:5000/estudios";
+  let url = "https://vessoni149.github.io/json-db-gestion-destudio/db.json";
 
 
   useEffect(() => {
